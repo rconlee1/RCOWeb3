e490d6ca5b3d01f2c52383e65a4ea74546a45516
rconl <rconl@DESKTOP-E8LN9OF>
2017-01-27 09:59:31 -0500

    many changes and added images

--- RCOWeb3/StudentPortal/courses.aspx.vb
+++ RCOWeb3/StudentPortal/courses.aspx.vb
@@ -1,5 +1,19 @@
-﻿
+﻿Imports Microsoft.AspNet.Identity
+Imports Microsoft.AspNet.Identity.EntityFramework
+Imports Microsoft.AspNet.Identity.Owin
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports Microsoft.Owin.Security
 Partial Class Courses
     Inherits System.Web.UI.Page
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim manager As New UserManager()
+        Dim user As New ApplicationUser()
+        Dim UName As String
+        UName = Session("UID")
+    End Sub
+
+
 
 End Class
